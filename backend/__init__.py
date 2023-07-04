8a679e86804c814877e3178cb251bed7e682e27b
Lim-Yi-Kiat <limyikiat97@hotmail.com>
2023-07-04 14:05:30 +0800

    error with set solved

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,10 +5,16 @@ from .extensions import db, migrate
 from .routes.order import main
 from .routes.user import user
 
+from dotenv import load_dotenv
+import os
+
+# load all environment variables
+load_dotenv()
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:accesstech@accesstech.cuyv9atug6pr.us-east-1.rds.amazonaws.com:5432/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
